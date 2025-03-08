d5efb66e40f655d9a3b1a081436bd399f3ca2d74
Muhammad Vidi <vidim@impstudio.co.id>
2025-03-08 13:17:28 +0700

    use env to run server

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import 'dotenv/config'
 // https://vite.dev/config/
+console.log(process.env)
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
+    port: process.env.VITE_PORT
   }
 })
