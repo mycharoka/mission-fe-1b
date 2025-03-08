a0271da0a87e6aec7b148b009639db412d27aa80
Muhammad Vidi <vidim@impstudio.co.id>
2025-03-08 13:25:28 +0700

    remove unused comment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import 'dotenv/config'
 // https://vite.dev/config/
-console.log(process.env)
 export default defineConfig({
   plugins: [react()],
   server: {
