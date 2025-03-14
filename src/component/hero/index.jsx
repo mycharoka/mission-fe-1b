9ba7360501a07a5dc2d2b3b4c7158dd4498e927d
Muhammad Vidi <vidim@impstudio.co.id>
2025-03-14 15:26:06 +0700

    fix responsive mobile homepage, hero & navbar

--- src/component/hero/index.jsx
+++ src/component/hero/index.jsx
@@ -1,31 +1,29 @@
 export function Hero() {
   return (
-    <section>
-      <img src="../../../src/assets/ao_ashi.jpg" alt="" className="w-full h-[70%] object-cover object-top mt-[90px]"/>
+    <section className="">
+      <img src="../../../src/assets/ao_ashi.jpg" alt="" className="w-full h-[500px] object-cover object-top mt-[90px] max-md:h-full max-md:mt-[50px]"/>
 
-    {/* <div className="flex flex-col gap-[10px] w-screen relative mt-[-150px] h-[600px] bg-[linear-gradient(to top, var(pageheaderbackground), transparent)] content-end">
-      
-    </div> */}
-    <div className="flex flex-col gap-[10px] w-screen relative mt-[-300px] px-[80px] pb-[80px] bg-heroGradient">
-        <div className="w-auto">
-          <p className="font-bold text-textlightprimary text-[48px] leading-[52.8px]">Ao Ashi</p>
-          <p className="font-medium text-[18px] leading-[25.2px] tracking-[0.2px] text-textlightprimary w-[668px] mt-[20px]">In an unfamiliar setting surrounded by talent, Ashito must bring out the best of his ability to prove himself and secure what could be a life-changing career.</p>
-        </div>
-        
-        <div className="flex  w-auto justify-between mt-[40px]">
-          <div className="flex gap-[10px]">
-            <button className="bg-backgroundprimary300 rounded-[48px] py-[10px] px-[26px] font-bold text-[16px] text-textlightprimary leading-[22.4px] tracking-[0.2px] border-none">Mulai</button>
-            <button className="rounded-[48px] bg-paperbackground py-[10px] px-[26px] font-bold leading-[22.4px] tracking-[0.2px] text-textlightprimary border-none flex items-center content-center gap-[10px]">
-              <span className="text-textlightprimary text-[20px]">ⓘ</span>
-              <span>Selengkapnya</span>
-            </button>
-            <p className="border-[1px] border-solid border-textlightprimary rounded-[24px] p-[10px] gap-[10px] w-[52px] font-normal text-[18px] leading-[25.2px] tracking-[0.2px] text-textlightsecondary text-center">14</p>
+      <div className="flex flex-col gap-[10px] w-full relative mt-[-500px] h-[500px] px-[80px] pb-[80px] z-50  bg-heroGradient justify-end max-md:pl-[22px] max-md:pr-[18px] max-md:mt-[-800px] max-md:h-[800px] max-md:pb-[15px]">
+
+          <div className="w-auto  max-md:mt-[600px] max-md:pt">
+            <p className="font-bold text-textlightprimary text-[48px] max-md:text-[24px] ">Ao Ashi</p>
+            <p className="font-medium text-[18px] tracking-[0.2px] text-textlightprimary w-[668px] mt-[20px] max-md:text-[12px] max-md:mt-[5px] max-md:w-[200px] ">In an unfamiliar setting surrounded by talent, Ashito must bring out the best of his ability to prove himself and secure what could be a life-changing career.</p>
+          </div>
+          
+          <div className="flex  w-auto justify-between mt-[40px] max-md:mt-[5px]">
+            <div className="flex gap-[10px]  max-md:gap-[8px]">
+              <button className="bg-backgroundprimary300 rounded-[48px] py-[10px] px-[26px] font-bold text-[16px] text-textlightprimary leading-[22.4px] tracking-[0.2px] border-none max-md:px-[7px] max-md:py-[3px] max-md:text-[12px] max-md:rounded-[20px]">Mulai</button>
+              <button className="rounded-[48px] bg-paperbackground py-[10px] px-[26px] font-bold leading-[22.4px] tracking-[0.2px] text-textlightprimary border-none flex items-center content-center gap-[10px] max-md:px-[7px] max-md:py-[3px] max-md:text-[12px] max-md:gap-[6px]">
+                <span className="text-textlightprimary text-[20px]">ⓘ</span>
+                <span>Selengkapnya</span>
+              </button>
+              <p className="border-[1px] border-solid border-textlightprimary rounded-[24px] p-[10px] gap-[10px] w-[52px] font-normal text-[18px] tracking-[0.2px] text-textlightsecondary text-center max-md:p[5px] max-md:text-[12px] max-md:rounded-[20px]">14</p>
+            </div>
+
+            <img src="../../../src/assets/muteIcon.png" alt="mute" className="bg-transparent bg-none max-md:hidden "/>
           </div>
 
-          <img src="../../../src/assets/muteIcon.png" alt="mute" className="bg-transparent bg-none"/>
-        </div>
-
-    </div>
+      </div>
     
     </section>
   )
--- src/component/navbar/index.jsx
+++ src/component/navbar/index.jsx
@@ -1,24 +1,9 @@
-// export function Navbar() {
-//   return (
-
-//       <nav className="bg-pageheaderbackground fixed w-screen flex items-center justify-between z-[999]">
-//         <div className="font-medium text-[18px] leading-[25.2px] tracking-[0.2px] my-[25px] ml-[80px] flex justify-between items-center w-[550.5px] h-[44px]">
-//           <img src="../../src/assets/Logo.png" alt="" />
-//           <a href="" className="text-textlightprimary no-underline">Series</a>
-//           <a href="" className="text-textlightprimary no-underline">Film</a>
-//           <a href="" className="text-textlightprimary no-underline">Daftar Saya</a>
-//         </div>
-//       </nav>
-
-//   )
-// }
-
 export function Navbar() {
   return (
-    <>
-    <header className="w-full z-[999] bg-pageheaderbackground fixed">
-    <nav className="flex justify-between items-center px-4 md:px-20 py-[25px]">
-      <div className="flex items-center gap-20">
+
+    <header className="bg-pageheaderbackground w-full z-[999] fixed px-[80px] py-[25px] max-md:px-[20px] max-md:py-[6px] max-md:h-[56px] max-md:w-screen max-md:z-[999] ">
+    <nav className="flex justify-between items-center  max-md:w-[350px]  max-md:py-[8px]">
+      <div className="items-center flex gap-20 max-md:gap-[12px]">
         <img 
           src="../../../src/assets/Logo.png" 
           alt="chill" 
@@ -27,21 +12,20 @@ export function Navbar() {
         <img
           src="../../../src/assets/mobile-logo.png"
           alt="chill-mobile"
-          className="w-8 md:hidden"
+          className="w-[20px] h-[18px] md:hidden"
         />
-        <div className="hidden md:flex gap-20">
-          <a className="text-textlightprimary font-medium hover:text-primary-300 text-[18px]">Series</a>
-          <a className="text-textlightprimary font-medium hover:text-primary-300 text-[18px]">Film</a>
-          <a className="text-textlightprimary font-medium hover:text-primary-300 text-[18px]">Daftar Saya</a>
-        </div>
+        <a className="text-textlightprimary font-medium hover:text-primary-300 text-[18px] max-md:text-[10px]">Series</a>
+        <a className="text-textlightprimary font-medium hover:text-primary-300 text-[18px] max-md:text-[10px]">Film</a>
+        <a className="text-textlightprimary font-medium hover:text-primary-300 text-[18px] max-md:text-[10px]">Daftar Saya</a>
+
       </div>
-      <img 
-        src="../../../src/assets/Avatar.png" 
-        alt="avatar" 
-        className="w-16 md:w-20"
-      />
+        <img 
+          src="../../../src/assets/Avatar.png" 
+          alt="avatar" 
+          className="w-[80px] h-auto max-md:w-[40px] max-md:mr-[20px]"
+        />
     </nav>
   </header>
-  </>
+
   )
 }
