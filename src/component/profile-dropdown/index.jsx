c7219b9dfa6014c834c803abe7ccafecfc2791c6
Muhammad Vidi <vidim@impstudio.co.id>
2025-03-27 16:34:03 +0700

    add hover for profile dropdown

--- src/component/profile-dropdown/index.jsx
+++ src/component/profile-dropdown/index.jsx
@@ -1,9 +1,9 @@
 export function ProfileDropdown() {
   return (
     <ul className="absolute right-0 translate-y-24 -translate-x-16 bottom-0 top-auto bg-pageheaderbackground">
-      <li className="flex gap-[5px] py-[8px] px-[12px] text-textlightprimary text-[14px] font-medium"><img src="../../../src/assets/dropdown-profile/account.svg" alt="" /><a href="/profile">Profil Saya</a></li>
-      <li className="flex gap-[5px] py-[8px] px-[12px] text-textlightprimary text-[14px] font-medium"><img src="../../../src/assets/dropdown-profile/star.svg" alt="" />Ubah Premium</li>
-      <li className="flex gap-[5px] py-[8px] px-[12px] text-textlightprimary text-[14px] font-medium"><img src="../../../src/assets/dropdown-profile/login-variant.svg" alt="" />Keluar</li>
+      <li className="flex gap-[5px] py-[8px] px-[12px] text-textlightprimary text-[14px] font-medium hover:bg-gray-700"><img src="../../../src/assets/dropdown-profile/account.svg" alt="" /><a href="/profile">Profil Saya</a></li>
+      <li className="flex gap-[5px] py-[8px] px-[12px] text-textlightprimary text-[14px] font-medium hover:bg-gray-700"><img src="../../../src/assets/dropdown-profile/star.svg" alt="" />Ubah Premium</li>
+      <li className="flex gap-[5px] py-[8px] px-[12px] text-textlightprimary text-[14px] font-medium hover:bg-gray-700"><img src="../../../src/assets/dropdown-profile/login-variant.svg" alt="" />Keluar</li>
     </ul>
   )
 }
