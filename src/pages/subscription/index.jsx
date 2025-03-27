2821e2abcb3521b6a0364900848dccea42917793
Muhammad Vidi <vidim@impstudio.co.id>
2025-03-27 16:28:17 +0700

    subscription page

--- src/pages/subscription/index.jsx
+++ src/pages/subscription/index.jsx
@@ -2,56 +2,6 @@ import {Navbar} from "../../component/navbar/index.jsx";
 import {Footer} from "../../component/footer/index.jsx";
 
 export default function SubscriptionPage() {
-    // return (
-    //     <>
-    //         <Navbar/>
-    //         <section className="pt-[1px] px-[80px]">
-    //             {/*benefit langganan*/}
-    //             <div className="mt-[150px] border-yellow-300 border-2">
-    //                 <div className="flex justify-center">
-    //                     <h3 className="font-bold text-[32px] text-textlightprimary">Kenapa Harus Berlangganan?</h3>
-    //                 </div>
-    //
-    //                 <div className="border-red-500 border-2 mt-[72px]">
-    //                     <div className="flex justify-around">
-    //                         <div className="items-center flex flex-col">
-    //                             <img src="../../../src/assets/subscription/download.png"/>
-    //                             <h5 className="font-bold text-[20px] text-textlightsecondary">Download Konten
-    //                                 Pilihan</h5>
-    //                         </div>
-    //                         <div className="items-center flex flex-col">
-    //                             <img src="../../../src/assets/subscription/advertisements-off.png"/>
-    //                             <h5 className="font-bold text-[20px] text-textlightsecondary">Tidak Ada Iklan</h5>
-    //                         </div>
-    //                         <div className="items-center flex flex-col">
-    //                             <img src="../../../src/assets/subscription/movie-roll.png"/>
-    //                             <h5 className="font-bold text-[20px] text-textlightsecondary">Tonton Semua Konten</h5>
-    //                         </div>
-    //                     </div>
-    //
-    //                     <div className="flex justify-around mt-[40px]">
-    //                         <div className="items-center flex flex-col">
-    //                             <img src="../../../src/assets/subscription/download.png"/>
-    //                             <h5 className="font-bold text-[20px] text-textlightsecondary">Kualitas Maksimal Sampai Dengan 4K</h5>
-    //                         </div>
-    //                         <div className="items-center flex flex-col">
-    //                             <img src="../../../src/assets/subscription/advertisements-off.png"/>
-    //                             <h5 className="font-bold text-[20px] text-textlightsecondary">Tidak Ada Iklan</h5>
-    //                         </div>
-    //                         <div className="items-center flex flex-col">
-    //                             <img src="../../../src/assets/subscription/movie-roll.png"/>
-    //                             <h5 className="font-bold text-[20px] text-textlightsecondary">Tonton Semua Konten</h5>
-    //                         </div>
-    //                     </div>
-    //                 </div>
-    //             </div>
-    //
-    //             {/*pilihan paket langganan*/}
-    //             <div></div>
-    //         </section>
-    //         {/*<Footer/>*/}
-    //     </>
-    // )
     return (
         <>
             <Navbar/>
@@ -87,6 +37,75 @@ export default function SubscriptionPage() {
                     </div>
                 </div>
             </section>
+
+            <section className="bg-paperbackground py-10">
+                <h3 className="text-white text-center font-bold text-[32px] mb-6">Pilih Paketmu</h3>
+                <p className="text-gray-400 text-center font-medium text-[18px] mb-12">Tentukan paket sesuai kebutuhanmu!</p>
+
+                <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mx-4 lg:mx-80 rounded-16px">
+                    <div className="bg-gradient-to-b from-blue-700 to-blue-500 p-6 rounded-16px text-center text-white">
+                        <span className="inline-block bg-gray-800 px-4 py-2 rounded-[999px] font-semibold mb-4">Individu</span>
+                        <p className="mb-6">Mulai dari Rp 49.999/bulan<br/>1 Akun</p>
+                        <ul className="space-y-2 mb-6">
+                            <li className="flex items-center justify-center">
+                                <span className="mr-2">✔️</span>
+                                Tidak Ada Iklan
+                            </li>
+                            <li className="flex items-center justify-center">
+                                <span className="mr-2">✔️</span>
+                                Kualitas 720p
+                            </li>
+                            <li className="flex items-center justify-center">
+                                <span className="mr-2">✔️</span>
+                                Download konten pilihan
+                            </li>
+                        </ul>
+                        <button className="bg-white text-blue-700 px-6 py-2 rounded-[999px] font-semibold mb-2">Langganan</button>
+                        <p className="text-[12px] text-white">Syaran & ketentuan berlaku</p>
+                    </div>
+                    <div className="bg-gradient-to-b from-blue-700 to-blue-500 p-6 rounded-16px text-center text-white">
+                        <span className="inline-block bg-gray-800 px-4 py-2 rounded-[999px] font-semibold mb-4">Berdua</span>
+                        <p className="mb-6">Mulai dari Rp 79.999/bulan<br/>2 Akun</p>
+                        <ul className="space-y-2 mb-6">
+                            <li className="flex items-center justify-center">
+                                <span className="mr-2">✔️</span>
+                                Tidak Ada Iklan
+                            </li>
+                            <li className="flex items-center justify-center">
+                                <span className="mr-2">✔️</span>
+                                Kualitas 1080p
+                            </li>
+                            <li className="flex items-center justify-center">
+                                <span className="mr-2">✔️</span>
+                                Download konten pilihan
+                            </li>
+                        </ul>
+                        <button className="bg-white text-blue-700 px-6 py-2 rounded-[999px] font-semibold mb-2">Langganan</button>
+                        <p className="text-[12px] text-white">Syaran & ketentuan berlaku</p>
+                    </div>
+                    <div className="bg-gradient-to-b from-blue-700 to-blue-500 p-6 rounded-16px text-center text-white">
+                        <span className="inline-block bg-gray-800 px-4 py-2 rounded-[999px] font-semibold mb-4">Keluarga</span>
+                        <p className="mb-6">Mulai dari Rp 159.999/bulan<br/>5-7 Akun</p>
+                        <ul className="space-y-2 mb-6">
+                            <li className="flex items-center justify-center">
+                                <span className="mr-2">✔️</span>
+                                Tidak Ada Iklan
+                            </li>
+                            <li className="flex items-center justify-center">
+                                <span className="mr-2">✔️</span>
+                                Kualitas 4K
+                            </li>
+                            <li className="flex items-center justify-center">
+                                <span className="mr-2">✔️</span>
+                                Download konten pilihan
+                            </li>
+                        </ul>
+                        <button className="bg-white text-blue-700 px-6 py-2 rounded-[999px] font-semibold mb-2">Langganan</button>
+                        <p className="text-[12px] text-white">Syaran & ketentuan berlaku</p>
+                    </div>
+                </div>
+            </section>
+            <Footer/>
         </>
     )
 }
