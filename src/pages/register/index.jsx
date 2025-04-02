5dda4f6df654b69810c24ca87511438368e27664
Muhammad Vidi <vidim@impstudio.co.id>
2025-04-02 22:31:00 +0700

    move to public directory

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -20,7 +20,7 @@ function RegisterPage() {
   return (
     <section className="bg-registerBackground bg-cover bg-no-repeat bg-center flex justify-center items-center h-screen w-screen">
       <form onSubmit={(e) => handleRegister(e)} className="bg-[rgba(24,26,28,0.84)] w-[529px] h-auto rounded-[16px] p-10 flex flex-col items-center max-md:w-[306px] max-md:p-6">
-        <img src="../../../src/assets/Logo.png" alt="chillflix" className="w-[163px] h-auto mb-[37px] max-md:h-[24px] max-md:w-[94px] max-md:mb-[20px]"/>
+        <img src="/assets/Logo.png" alt="chillflix" className="w-[163px] h-auto mb-[37px] max-md:h-[24px] max-md:w-[94px] max-md:mb-[20px]"/>
         <h3 className="text-[32px] font-bold leading-[35.2px] text-textlightprimary tracking-[0.2px] max-md:text-[18px]">Daftar</h3>
         <p className="text-textlightprimary font-normal text-[16px] leading-[22.4px] tracking-[0.2px] mb-[37px] max-md:mb-[20px] max-md:text-[10px]">Selamat Datang!</p>
 
